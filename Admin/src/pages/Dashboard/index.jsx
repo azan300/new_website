af571fdce23e641d656dda7051bca0dccd0cf0fb
Andres Sandoval <azan300@gmail.com>
2025-03-25 17:34:49 -0500

    Trying to set up the dashboard all purdy

--- Admin/src/pages/Dashboard/index.jsx
+++ Admin/src/pages/Dashboard/index.jsx
@@ -1,71 +1,34 @@
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Card,
-  CardBody,
-  Input,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Table,
-} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
-import classNames from "classnames";
+// Import Widgets
+import WelcomeWidget from "../../components/WelcomeWidget";
+import CalendarWidget from "../../components/CalendarWidget";
+import ChatWidget from "../../components/ChatWidget";
+import EmailWidget from "../../components/EmailWidget";
+import ProjectWidget from "../../components/ProjectWidget";
 
-//import Charts
-import StackedColumnChart from "./StackedColumnChart";
-
-//import action
-import { getChartsData as onGetChartsData } from "/src/store/actions";
-
-import modalimage1 from "../../assets/images/product/img-7.png";
-import modalimage2 from "../../assets/images/product/img-4.png";
-
-// Pages Components
-import WelcomeComp from "./WelcomeComp";
-import MonthlyEarning from "./MonthlyEarning";
-import SocialSource from "./SocialSource";
-import ActivityComp from "./ActivityComp";
-import TopCities from "./TopCities";
-
-//Import Breadcrumb
+// Import Breadcrumb
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 
-//i18n
-import { withTranslation } from "react-i18next";
-
-//redux
+// Redux
 import { useSelector, useDispatch } from "react-redux";
 import { createSelector } from "reselect";
-import LatestTranactions from "./LatestTranaction";
+import { getChartsData as onGetChartsData } from "/src/store/actions";
 
 const Dashboard = (props) => {
-
-  const [modal, setmodal] = useState(false);
-  const [subscribemodal, setSubscribemodal] = useState(false);
+  const dispatch = useDispatch();
 
   const DashboardProperties = createSelector(
     (state) => state.Dashboard,
     (dashboard) => ({
-      chartsData: dashboard.chartsData
+      chartsData: dashboard.chartsData,
     })
   );
 
-  const {
-    chartsData
-  } = useSelector(DashboardProperties);
-
-  const reports = [
-    { title: "Orders", iconClass: "bx-copy-alt", description: "1,235" },
-    { title: "Revenue", iconClass: "bx-archive-in", description: "$35, 723" },
-    { title: "Average Price", iconClass: "bx-purchase-tag-alt", description: "$16.2" },
-  ];
+  const { chartsData } = useSelector(DashboardProperties);
 
   const [periodData, setPeriodData] = useState([]);
   const [periodType, setPeriodType] = useState("Year");
@@ -74,221 +37,54 @@ const Dashboard = (props) => {
     setPeriodData(chartsData);
   }, [chartsData]);
 
-  const onChangeChartPeriod = pType => {
-    setPeriodType(pType);
-    dispatch(onGetChartsData(pType));
-  };
-
-  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(onGetChartsData("Year"));
   }, [dispatch]);
 
-  //meta title
-  document.title = "Dashboard | StraightUp Roofing - Vite React Admin & Dashboard Template";
+  // Meta title
+  document.title = "Dashboard | Skote Admin Panel";
 
   return (
     <React.Fragment>
       <div className="page-content">
         <Container fluid>
-          {/* Render Breadcrumb */}
+          {/* Breadcrumb */}
           <Breadcrumbs title={props.t("Dashboards")} breadcrumbItem={props.t("Dashboard")} />
 
+          {/* Row 1: Welcome & Projects */}
           <Row>
             <Col xl="4">
-              <WelcomeComp />
-              <MonthlyEarning />
+              <WelcomeWidget />
             </Col>
             <Col xl="8">
-              <Row>
-                {/* Reports Render */}
-                {(reports || [])?.map((report, key) => (
-                  <Col md="4" key={"_col_" + key}>
-                    <Card className="mini-stats-wid">
-                      <CardBody>
-                        <div className="d-flex">
-                          <div className="flex-grow-1">
-                            <p className="text-muted fw-medium">
-                              {report.title}
-                            </p>
-                            <h4 className="mb-0">{report.description}</h4>
-                          </div>
-                          <div className="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
-                            <span className="avatar-title rounded-circle bg-primary">
-                              <i className={"bx " + report.iconClass + " font-size-24"}></i>
-                            </span>
-                          </div>
-                        </div>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-
-              <Card>
-                <CardBody>
-                  <div className="d-sm-flex flex-wrap">
-                    <h4 className="card-title mb-4">Email Sent</h4>
-                    <div className="ms-auto">
-                      <ul className="nav nav-pills">
-                        <li className="nav-item">
-                          <Link to="#" className={classNames({ active: periodType === "Week" }, "nav-link")} onClick={() => { onChangeChartPeriod("Week"); }} id="one_month">
-                            Week
-                          </Link>{" "}
-                        </li>
-                        <li className="nav-item">
-                          <Link to="#" className={classNames({ active: periodType === "Month" }, "nav-link")} onClick={() => { onChangeChartPeriod("Month"); }} id="one_month">
-                            Month
-                          </Link>
-                        </li>
-                        <li className="nav-item">
-                          <Link to="#" className={classNames({ active: periodType === "Year" }, "nav-link")} onClick={() => { onChangeChartPeriod("Year"); }} id="one_month">
-                            Year
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  {/* <div className="clearfix"></div> */}
-                  <StackedColumnChart periodData={periodData} dataColors='["--bs-primary", "--bs-warning", "--bs-success"]' />
-                </CardBody>
-              </Card>
+              <ProjectWidget />
             </Col>
           </Row>
 
+          {/* Row 2: Calendar & Chat */}
           <Row>
-            <Col xl="4">
-              <SocialSource />
+            <Col xl="6">
+              <CalendarWidget />
             </Col>
-            <Col xl="4">
-              <ActivityComp />
-            </Col>
-
-            <Col xl="4">
-              <TopCities />
+            <Col xl="6">
+              <ChatWidget />
             </Col>
           </Row>
 
+          {/* Row 3: Email */}
           <Row>
-            <Col lg="12">
-              <LatestTranactions />
+            <Col xl="12">
+              <EmailWidget />
             </Col>
           </Row>
         </Container>
       </div>
-
-      <Modal
-        isOpen={modal}
-        role="dialog"
-        autoFocus={true}
-        centered={true}
-        className="exampleModal"
-        tabIndex="-1"
-        toggle={() => {
-          setmodal(!modal);
-        }}
-      >
-        <div>
-          <ModalHeader
-            toggle={() => {
-              setmodal(!modal);
-            }}
-          >
-            Order Details
-          </ModalHeader>
-          <ModalBody>
-            <p className="mb-2">
-              Product id: <span className="text-primary">#SK2540</span>
-            </p>
-            <p className="mb-4">
-              Billing Name: <span className="text-primary">Neal Matthews</span>
-            </p>
-
-            <div className="table-responsive">
-              <Table className="table table-centered table-nowrap">
-                <thead>
-                  <tr>
-                    <th scope="col">Product</th>
-                    <th scope="col">Product Name</th>
-                    <th scope="col">Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">
-                      <div>
-                        <img src={modalimage1} alt="" className="avatar-sm" />
-                      </div>
-                    </th>
-                    <td>
-                      <div>
-                        <h5 className="text-truncate font-size-14">
-                          Wireless Headphone (Black)
-                        </h5>
-                        <p className="text-muted mb-0">$ 225 x 1</p>
-                      </div>
-                    </td>
-                    <td>$ 255</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">
-                      <div>
-                        <img src={modalimage2} alt="" className="avatar-sm" />
-                      </div>
-                    </th>
-                    <td>
-                      <div>
-                        <h5 className="text-truncate font-size-14">
-                          Hoodie (Blue)
-                        </h5>
-                        <p className="text-muted mb-0">$ 145 x 1</p>
-                      </div>
-                    </td>
-                    <td>$ 145</td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <h6 className="m-0 text-end">Sub Total:</h6>
-                    </td>
-                    <td>$ 400</td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <h6 className="m-0 text-end">Shipping:</h6>
-                    </td>
-                    <td>Free</td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <h6 className="m-0 text-end">Total:</h6>
-                    </td>
-                    <td>$ 400</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              type="button"
-              color="secondary"
-              onClick={() => {
-                setmodal(!modal);
-              }}
-            >
-              Close
-            </Button>
-          </ModalFooter>
-        </div>
-      </Modal>
     </React.Fragment>
   );
 };
 
 Dashboard.propTypes = {
   t: PropTypes.any,
-  chartsData: PropTypes.any,
-  onGetChartsData: PropTypes.func,
 };
 
-export default withTranslation()(Dashboard);
+export default Dashboard;
